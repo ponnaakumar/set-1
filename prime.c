3c38b979efa5716d690ddeb9f93b7d2bb289647e
ponnaakumar <37841804+ponnaakumar@users.noreply.github.com>
2018-03-29 18:23:10 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+main()
+{
+   int n;
+   scanf("%d",&n);
+      if(n%2==0)
+      {
+         printf("%d isn't prime.\n", n);
+      }
+      else
+      {
+      	printf("prime");
+      }
+
+   return 0;
+}
